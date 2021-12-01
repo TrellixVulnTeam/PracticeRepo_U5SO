4792a2983803ca3130245e3030252048a2745b29
luka <lukaturkia@gmail.com>
2021-12-01 21:18:36 +0400

    final

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteinputElement = document.getElementById('quoteInput')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
-quoteinputElement.addEventListener('input', () => {
-    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-    const arrayValue = quoteinputElement.value.split('')
-    arrayQuote.forEach((characterSpan,index) =>{
-        const character = arrayValue[index]
-        if (character === characterSpan.innerText){
-            characterSpan.classList.add('correct')
-            characterSpan.classList.remove('incorrect')
-        }
-        else {
-            characterSpan.classList.remove('correct')
-            characterSpan.classList.add('incorrect')
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
 
-        }
-    })
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
+
+  if (correct) renderNewQuote()
 })
-function getRandomQuote(){
-return fetch(RANDOM_QUOTE_API_URL)
-.then(response => response.json())
-.then(data => data.content)
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
 }
 
-async function renderNextQuote(){
-    const quote = await getRandomQuote()
-    quoteDisplayElement.innerText = quote
-    quote.split('').forEach(character => {
-        const characterSpan = document.createElement('span')
-        characterSpan.innerText =character 
-        characterSpan.classList.add('correct')
-        quoteDisplayElement.appendChild(characterSpan)
-    })
-    quoteinputElement.value = null
+async function renderNewQuote() {
+  const quote = await getRandomQuote()
+  quoteDisplayElement.innerHTML = ''
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
+  startTimer()
 }
-renderNextQuote()
+
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
