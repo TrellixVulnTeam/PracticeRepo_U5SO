3a5e7cc0b9046a36f64abf0e85d4357f11f5e3ae
luka <lukaturkia@gmail.com>
2021-12-01 20:49:43 +0400

    starting of js json quote grab for this

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("WHAT IS THIS?")
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById(quoteDisplay)
+function getRandomQuote(){
+return fetch(RANDOM_QUOTE_API_URL)
+.then(response => response.json())
+.then(data => data.content)
+}
+
+async function renderNextQuote(){
+    const quote = await getRandomQuote()
+}
+renderNextQuote()
