c3fe42feeb76c9b8c2ba17c3719d7a8b4aea9b24
luka <lukaturkia@gmail.com>
2021-12-01 21:04:42 +0400

    adding correct and wrong 2

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteinputElement = document.getElementById('quoteInput')
 
+quoteinputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteinputElement.value.split('')
+    arrayQuote.forEach((characterSpan,index) =>{
+        const character = arrayValue[index]
+        if (character === characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        }
+        else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
 
+        }
+    })
+})
 function getRandomQuote(){
 return fetch(RANDOM_QUOTE_API_URL)
 .then(response => response.json())
